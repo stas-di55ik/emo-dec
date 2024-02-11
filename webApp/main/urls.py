d93e93f1b86e7715d2555ccfc7dbe60619cc36a6
stas-di55ik <stas.hanchev@gmail.com>
2024-02-11 22:53:13 +0200

    added all pages

--- webApp/main/urls.py
+++ webApp/main/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('contacts', views.contacts, name='contacts'),
+    path('faceEmotionDetection', views.faceEmotionDetection, name='faceEmotionDetection'),
+    path('textSentimentAnalysis', views.textSentimentAnalysis, name='textSentimentAnalysis'),
+    path('igPublicationAudit', views.igPublicationAudit, name='igPublicationAudit'),
 ]
--- webApp/main/views.py
+++ webApp/main/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'main/index.html')
 
+
 def about(request):
     return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+
+def faceEmotionDetection(request):
+    return render(request, 'main/faceEmotionDetection.html')
+
+
+def textSentimentAnalysis(request):
+    return render(request, 'main/textSentimentAnalysis.html')
+
+
+def igPublicationAudit(request):
+    return render(request, 'main/igPublicationAudit.html')
