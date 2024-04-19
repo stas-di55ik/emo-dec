723a323bca9a6852e08c74611acc30897a0fca5b
stas-di55ik <stas.hanchev@gmail.com>
2024-04-19 23:30:56 +0300

    ig almost finished

--- webApp/main/urls.py
+++ webApp/main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('faceEmotionDetection', views.faceEmotionDetection, name='faceEmotionDetection'),
     path('textSentimentAnalysis', views.textSentimentAnalysis, name='textSentimentAnalysis'),
     path('igPublicationAudit', views.igPublicationAudit, name='igPublicationAudit'),
+    path('igPublicationAuditProfile', views.igPublicationAuditProfile, name='igPublicationAuditProfile'),
 ]
--- webApp/main/views.py
+++ webApp/main/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 from .forms import SentimentAnalysisSourceForm
 from .models import TextSentimentAnalyser, PhotoEmotionDetector, InstagramAPIHelper
 
 import os
+import requests
 from threading import Timer
 
 import datetime
 import math
-import time
 
 from .secrets import IG_APP_ID, IG_APP_SECRET
 
@@ -104,9 +106,66 @@ def igPublicationAudit(request):
     if request.method == 'POST':
         ig_helper.init_token(request)
         profile_data = ig_helper.get_profile_data()
-        print(profile_data)
+        request.session['profile_data'] = profile_data
+        return redirect('igPublicationAuditProfile')
 
-        return render(request, 'main/igPublicationAuditProfile.html', profile_data)
     data = {'login_url': login_url}
 
     return render(request, 'main/igPublicationAudit.html', data)
+
+
+def igPublicationAuditProfile(request):
+    profile_data = request.session.get('profile_data')
+    if request.method == 'POST':
+        timestamp = math.floor(datetime.datetime.now().timestamp() * 1000000)
+        input_publication_id = request.POST.get('publication_id')
+
+        existing = {}
+        for publication in profile_data['user_media']['data']:
+            if input_publication_id == publication['id']:
+                existing = publication
+
+        if existing['caption']:
+            tsa_result = TextSentimentAnalyser.analyze(existing['caption'], timestamp)
+            # Timer(15.0, lambda: os.remove(tsa_result['plot_path'])).start()
+
+            tsa_web_dict = {'result': tsa_result['result'],
+                            'plot_path': tsa_result['plot_path'].lstrip()[5:],
+                            'plot_succeed': bool(tsa_result['emotion_list']),
+                            'input_text': tsa_result['input_text']}
+        else:
+            tsa_result = False
+
+        image_paths = []
+        if existing['media_type'] == 'CAROUSEL_ALBUM':
+            for item in existing['children']['data']:
+                if item['media_type'] == 'IMAGE':
+                    file_path = f'main/static/main/tmp/photoAnalysis/req/{timestamp}_{item['id']}.jpg'
+                    download_status = download_image(item['media_url'], file_path)
+                    if download_status:
+                        image_paths.append(file_path)
+
+        if existing['media_type'] == 'IMAGE':
+            file_path = f'main/static/main/tmp/photoAnalysis/req/{timestamp}_{existing['id']}.jpg'
+            download_status = download_image(existing['media_url'], file_path)
+            if download_status:
+                image_paths.append(file_path)
+
+        print(f'image_paths: {image_paths}')
+
+    return render(request, 'main/igPublicationAuditProfile.html', profile_data)
+
+
+def download_image(url, save_path):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print(f"Image downloaded successfully to {save_path}")
+
+        return True
+    except Exception as e:
+        print(f"Error downloading image: {e}")
+
+        return False
